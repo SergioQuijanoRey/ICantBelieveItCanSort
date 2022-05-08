2f8b949030ceead37fb5228019c2cee3f4eb3310
Sergio <sergiquijano@gmail.com>
2022-05-08 18:19:20 +0200

    refactor: created a struct and moved logic to that struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
 
-/// The main sorting algorithm
-/// It creates a copy of the original values and returns that copy sorted
-/// It sorts in non-decreasing order
-pub fn ICantBelieveItCanSort<T>(values: Vec<T>) -> Vec<T>
-where T: Ord + Clone {
+/// Struct where we implement the sorting algorithms, but in different ways
+/// IE. inplace sort vs creating a clone and sorting that clone, without mutating given data
+struct ICantBelieveItCanSort;
 
-    // Start cloning the values into a mutable vector
-    let mut new_values = values.clone();
 
-    // Use the sorting algo
-    for i in 1..new_values.len(){
-        for j in 1..new_values.len(){
-            if new_values[i] < new_values[j]{
-                new_values.swap(i, j);
+impl ICantBelieveItCanSort{
+
+    /// Sort a given vector of values, without mutating the original vector of values
+    /// Thus, a clone is created, sorted and returned
+    pub fn sorted<T>(values: Vec<T>) -> Vec<T>
+    where T: Ord + Clone{
+        // Start cloning the values into a mutable vector
+        let mut new_values = values.clone();
+
+        // Use the sorting algo
+        for i in 1..new_values.len(){
+            for j in 1..new_values.len(){
+                if new_values[i] < new_values[j]{
+                    new_values.swap(i, j);
+                }
             }
         }
+
+        return new_values;
     }
 
-    return new_values;
 }
+
 
 
 #[cfg(test)]
@@ -45,30 +53,32 @@ mod tests {
 
     #[test]
     fn basic_cases() {
+
+
         let values = vec![6, 6, 5, 1, 2, 3, 9];
-        let sorted_values = ICantBelieveItCanSort(values);
+        let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
 
         let values = vec![3, 1, 4, 1, 5, 9, 2];
-        let sorted_values = ICantBelieveItCanSort(values);
+        let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
 
 
         let values = vec![3, 3, 3, 3, 3];
-        let sorted_values = ICantBelieveItCanSort(values);
+        let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
 
         let values = vec![1, 2, 3, 4, 5, 6];
-        let sorted_values = ICantBelieveItCanSort(values);
+        let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
 
 
         let values = vec![9, 5, 3, 2, 1];
-        let sorted_values = ICantBelieveItCanSort(values);
+        let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
 
         let values = vec![-1, -10, 2, -14, 15];
-        let sorted_values = ICantBelieveItCanSort(values);
+        let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
     }
 }
