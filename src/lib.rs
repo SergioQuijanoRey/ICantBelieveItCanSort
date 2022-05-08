c69baacd5fd59ca84e59e7dc10699e431fb32f63
Sergio <sergiquijano@gmail.com>
2022-05-08 18:23:01 +0200

    feat: now a in-place mutating sort is available

--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,19 @@ impl ICantBelieveItCanSort{
         return new_values;
     }
 
+    /// Sort a given vector in-place, mutating it
+    pub fn sort<T>(values: &mut Vec<T>)
+    where T: Ord{
+        // Use the sorting algo
+        for i in 1..values.len(){
+            for j in 1..values.len(){
+                if values[i] < values[j]{
+                    values.swap(i, j);
+                }
+            }
+        }
+    }
+
 }
 
 
@@ -52,8 +65,7 @@ mod tests {
     }
 
     #[test]
-    fn basic_cases() {
-
+    fn basic_cases_sorted() {
 
         let values = vec![6, 6, 5, 1, 2, 3, 9];
         let sorted_values = ICantBelieveItCanSort::sorted(values);
@@ -72,7 +84,6 @@ mod tests {
         let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
 
-
         let values = vec![9, 5, 3, 2, 1];
         let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
@@ -80,6 +91,36 @@ mod tests {
         let values = vec![-1, -10, 2, -14, 15];
         let sorted_values = ICantBelieveItCanSort::sorted(values);
         assert!(check_non_decreasing_sorted(sorted_values));
+    }
+
+    #[test]
+    fn basic_cases_sort(){
+
+        let mut values = vec![6, 6, 5, 1, 2, 3, 9];
+        ICantBelieveItCanSort::sort(&mut values);
+        assert!(check_non_decreasing_sorted(values));
+
+        let mut values = vec![3, 1, 4, 1, 5, 9, 2];
+        ICantBelieveItCanSort::sort(&mut values);
+        assert!(check_non_decreasing_sorted(values));
+
+        let mut values = vec![3, 3, 3, 3, 3];
+        ICantBelieveItCanSort::sort(&mut values);
+        assert!(check_non_decreasing_sorted(values));
+
+        let mut values = vec![1, 2, 3, 4, 5, 6];
+        ICantBelieveItCanSort::sort(&mut values);
+        assert!(check_non_decreasing_sorted(values));
+
+
+        let mut values = vec![9, 5, 3, 2, 1];
+        ICantBelieveItCanSort::sort(&mut values);
+        assert!(check_non_decreasing_sorted(values));
+
+        let mut values = vec![-1, -10, 2, -14, 15];
+        ICantBelieveItCanSort::sort(&mut values);
+        assert!(check_non_decreasing_sorted(values));
+
     }
 }
 
