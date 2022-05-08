c1e4e0d8e914485f83fda3f7093d8cc2a0efbebc
Sergio <sergiquijano@gmail.com>
2022-05-08 18:32:45 +0200

    feat: now ordering can be done with custom criteria

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,27 @@ impl ICantBelieveItCanSort{
         }
     }
 
+    /// Sort a given vector of values, without mutating the original vector of values
+    /// Thus, a clone is created, sorted and returned
+    /// `criteria(first, second)` replaces `first < second` comparison
+    pub fn sorted_by<T>(values: Vec<T>, criteria: fn(&T, &T) -> bool) -> Vec<T>
+    where T: Clone{
+        // Start cloning the values into a mutable vector
+        let mut new_values = values.clone();
+
+        // Use the sorting algo
+        for i in 1..new_values.len(){
+            for j in 1..new_values.len(){
+                if criteria(&new_values[i], &new_values[j]){
+                    new_values.swap(i, j);
+                }
+            }
+        }
+
+        return new_values;
+
+    }
+
 }
 
 
@@ -55,6 +76,23 @@ mod tests {
 
                 // Check for a possible failure
                 if values[first] > values[second]{
+                    return false;
+                }
+            }
+        }
+
+        // All checks passed
+        return true;
+    }
+
+    /// For checking that a vec is sorted in non-increasing order
+    fn check_non_increasing_sorted<T: Ord>(values: Vec<T>) -> bool{
+
+        for first in 1..values.len(){
+            for second in first..values.len(){
+
+                // Check for a possible failure
+                if values[first] < values[second]{
                     return false;
                 }
             }
@@ -121,6 +159,41 @@ mod tests {
         ICantBelieveItCanSort::sort(&mut values);
         assert!(check_non_decreasing_sorted(values));
 
+    }
+
+    #[test]
+    fn basic_cases_sorted_with_criteria() {
+
+        // The criteria we are going to use
+        // Invert the sorting order
+        fn criteria(first: &i32, second: &i32) -> bool{
+            return first > second;
+        }
+
+        let values = vec![6, 6, 5, 1, 2, 3, 9];
+        let sorted_values = ICantBelieveItCanSort::sorted_by(values, criteria);
+        assert!(check_non_increasing_sorted(sorted_values));
+
+        let values = vec![3, 1, 4, 1, 5, 9, 2];
+        let sorted_values = ICantBelieveItCanSort::sorted_by(values, criteria);
+        assert!(check_non_increasing_sorted(sorted_values));
+
+
+        let values = vec![3, 3, 3, 3, 3];
+        let sorted_values = ICantBelieveItCanSort::sorted_by(values, criteria);
+        assert!(check_non_increasing_sorted(sorted_values));
+
+        let values = vec![1, 2, 3, 4, 5, 6];
+        let sorted_values = ICantBelieveItCanSort::sorted_by(values, criteria);
+        assert!(check_non_increasing_sorted(sorted_values));
+
+        let values = vec![9, 5, 3, 2, 1];
+        let sorted_values = ICantBelieveItCanSort::sorted_by(values, criteria);
+        assert!(check_non_increasing_sorted(sorted_values));
+
+        let values = vec![-1, -10, 2, -14, 15];
+        let sorted_values = ICantBelieveItCanSort::sorted_by(values, criteria);
+        assert!(check_non_increasing_sorted(sorted_values));
     }
 }
 
